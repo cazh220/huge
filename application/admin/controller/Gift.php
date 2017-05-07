fcf2ac3dc4ae42f06b455f55d31d2bec361f63d4
cazh220@163.com <cazh220@163.com>
2017-05-07 22:42:02 +0800

    商品管理

--- application/admin/controller/Gift.php
+++ application/admin/controller/Gift.php
@@ -7,7 +7,10 @@ class Gift
 {
 	public function index()
     {
+		$Gift = model('Gift');
+		$data = $Gift->getGift();
         $view = new View();
+		$view->assign('data', $data);
 		return $view->fetch('index/gift/index');
     }
 	
@@ -15,5 +18,36 @@ class Gift
 	{
 		$view = new View();
 		return $view->fetch('index/gift/add');
+	}
+	
+	public function doAdd()
+	{
+		$gift_name = input('gift_name');
+		$gift_intro = input('gift_intro');
+		$credit = input('credit');
+
+		$param = array(
+			'gift_name'		=> $gift_name,
+			'gift_photo'	=> '',
+			'credits'		=> $credit,
+			'num'			=> 30,
+			'sales_num'		=> 123,
+			'create_time'	=> date("Y-m-d H:i:s", time()),
+			'status'		=> 0,
+			'update_time'	=> date("Y-m-d H:i:s", time()),
+			'validity_time'	=> date("Y-m-d H:i:s", time()),
+			'is_delete'		=> 0
+		);
+		$Gift = model('Gift');
+		$ressult = $Gift->addGift($param);
+		$url = $_SERVER['HTTP_ORIGIN']."/public/admin.php/admin/gift/index";
+		if ($ressult == 0)
+		{
+			echo "<script>alert('新建礼品失败');history.back();</script>";
+		}
+		else
+		{
+			echo "<script>windows.location.href='".$url."';</script>";
+		}
 	}
 }
---
+++ application/admin/model/Gift.php
@@ -0,0 +1,52 @@
+<?php
+namespace app\admin\model;
+
+use think\Model;
+use think\Db;
+
+class Gift extends Model
+{
+	public function addGift($data=array())
+	{
+		$res = Db::execute("INSERT INTO hg_gift SET gift_name = :gift_name, gift_photo = :gift_photo, credits = :credits, num = :num, sales_num = :sales_num,create_time = :create_time, status = :status, update_time = :update_time, validity_time = :validity_time, is_delete = :is_delete", $data);
+		
+		return $res;
+	}
+	
+	public function getGift($data=array())
+	{
+		$where_condition = "";
+		$where_ary = array();
+		//Æ´½Ó²ÎÊý
+		foreach($data as $key => $val)
+		{
+			array_push($where_ary, $key."= :".$key);
+		}
+		if (!empty($where_ary))
+		{
+			$where_condition = implode(' AND ', $where_ary);
+			if (!empty($where_condition))
+			{
+				$where_condition = " WHERE {$where_condition} ";
+			}
+		}
+
+		$order = " ORDER BY gift_id DESC";
+		if ($order=='1')
+		{
+			$order = " ORDER BY admin_id ASC";
+		}
+		$where_condition .= $order;
+		if (!empty($page))
+		{
+			$start = $page['current_page'] - 1;
+			$page_size = $page['page_size'];
+			$start = ($start > 0) ? intval($start) : 0;
+			$limit = " LIMIT $start, $page_size";
+			$where_condition .= $limit;
+		}
+		
+		$res = Db::query("select *  from hg_gift $where_condition", $data);
+		return $res;
+	}
+}
