a4d2711aada6210c14b28ac7343665843ec0987f
caozheng <caozheng@xinlonghang.cn>
2017-08-19 19:00:36 +0800

    huge qianduan

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,25 @@ use think\View;
 
 class Index
 {
+	public function index()
+	{
+		$view = new View();
+		return $view->fetch('index');
+	}
+	
+	public function login()
+	{
+		$view = new View();
+		return $view->fetch('login');
+	}
+	
+	public function register()
+	{
+		$view = new View();
+		return $view->fetch('register');
+	}
+	
+	/*
     public function index()
     {
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
@@ -13,8 +32,8 @@ class Index
 	
 	public function list_users()
 	{
-		$data = Db::query('select * from hg_users');
-		//print_r($data);
+		$data = Db::query('select * from hg_user');
+		print_r($data);
 		$view = new View();
 		$view->assign('data', $data);
 		//$view->name = 'caozheng';
@@ -45,4 +64,5 @@ class Index
 		$res = Db::execute('delete from hg_users where id = :id', ['id'=>7]);
 		var_dump($res);
 	}
+	*/
 }
