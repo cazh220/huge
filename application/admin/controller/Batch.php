545cd97f391eb6a10ece9f4b51ac44852c68d97e
cazh220@163.com <cazh220@163.com>
2017-08-30 02:42:21 +0800

    up

--- application/admin/controller/Batch.php
+++ application/admin/controller/Batch.php
@@ -53,6 +53,66 @@ class Batch
 		//print_r($data);die;
 	}
 	
+	
+	//读取excel
+	public function  readexcel()
+	{
+		require_once VENDOR_PATH.'PHPExcel.php';
+		//$PHPReader = new \PHPExcel();
+		$file_path = ROOT_PATH.'data\1.xlsx';
+		//echo $file_path;die;
+		/*
+		$PHPReader = new \PHPExcel_Reader_Excel2007();
+		if(!$PHPReader->canRead($file_path)){
+		    $PHPReader = new \PHPExcel_Reader_Excel5();
+		    
+		    if(!$PHPReader->canRead($file_path)){//print_r('xxx');die;
+		        echo 'no Excel';
+		        return ;
+		  }
+	}*/
+		
+		$extension = strtolower(pathinfo($file_path, PATHINFO_EXTENSION) );
+
+		if ($extension =='xlsx') {
+		    $PHPReader = new \PHPExcel_Reader_Excel2007();
+		    $PHPExcel = $PHPReader->load($file_path);
+		} else if ($extension =='xls') {
+		    $PHPReader = new \PHPExcel_Reader_Excel5();
+		    $PHPExcel = $PHPReader->load($file_path);
+		} else if ($extension=='csv') {
+		    $PHPReader = new \PHPExcel_Reader_CSV();
+		
+		    //默认输入字符集
+		    $PHPReader->setInputEncoding('GBK');
+		
+		    //默认的分隔符
+		    $PHPReader->setDelimiter(',');
+		
+		    //载入文件
+		    $PHPExcel = $PHPReader->load($file_path);
+		}
+		
+		
+		//$PHPExcel = $PHPReader->load($file_path);
+		/**读取excel文件中的第一个工作表*/
+		$currentSheet = $PHPExcel->getSheet(0);
+		/**取得最大的列号*/
+		$allColumn = $currentSheet->getHighestColumn();
+		/**取得一共有多少行*/
+		$allRow = $currentSheet->getHighestRow();
+		//echo $allRow;die;
+		for($currentRow =2;$currentRow <= $allRow;$currentRow++){
+		/**从第A列开始输出*/
+			for($currentColumn= 'A';$currentColumn<= $allColumn; $currentColumn++){
+    				$val = $currentSheet->getCellByColumnAndRow(ord($currentColumn) - 65,$currentRow)->getValue();/**ord()将字符转为十进制数*/
+    				echo $val;echo "<br>";
+    			}
+    		}
+				
+		
+		
+	}
 }
 	
 	
